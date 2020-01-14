962a57ad0245de14fdd5b7d917c63858a7096acd
Lovepreet <lovepreet.singh@medtrail.in>
2020-01-14 13:20:17 +0530

    add mongodb functionality

--- app.js
+++ app.js
@@ -43,10 +43,6 @@ app.use((req, res, next) => {
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use((err, req, res, next) => {
         res.status(err.status || 500);
@@ -57,8 +53,6 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
--- routes/api_router.js
+++ routes/api_router.js
@@ -5,32 +5,23 @@
 const express = require('express');
 
 const router = express.Router();
-
-// var momentDurationFormatSetup = require("moment-duration-format");
-// const deferred = require('../lib/common-utils/deferred');
 const fn = require('../lib/common-utils/functions');
 const {callAPI} = require('../lib/common-utils/router_functions');
 const webAPI = require('../lib/apis/webAPI').getInstance();
-// const crawl = require('../scripts/onemg/crawler').crawl
-// const crawlBatch = require('../scripts/onemg/crawl_page').crawlBatch
-
-/* GET home page. */
-
-// const CONFIG = require('config');
-// const httpClient = require('../lib/clients/httpclient.js').getInstance();
-// const cronInstance = require('../crons/index').getInstance();
-// cronInstance.startCrons(['monthlyAnalyticsNotif','dailyAnalyticsNotif','weeklyAnalyticsNotif']);
-
-
-// const elasticsearchClient = require('../lib/clients/elasticsearchClient.js').getInstance();
-
-/**
- * Web APIS
- */
 
 router.get('/v1/testing', (req, res) => {
   callAPI(req, res, fn.bind(webAPI, 'randomTesting'));
 });
 
+router.post('/v1/getDataById', (req, res) => {
+  callAPI(req, res, fn.bind(webAPI, 'findDocById'));
+});
 
+router.post('/v1/insertDataInDb', (req, res) => {
+  callAPI(req, res, fn.bind(webAPI, 'insertDataInDb'));
+});
+
+router.post('/v1/updateDataInDb', (req, res) => {
+  callAPI(req, res, fn.bind(webAPI, 'updateDoc'));
+});
 module.exports = router;
